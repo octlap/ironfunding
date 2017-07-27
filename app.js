84cc62511fcf1ac658c559af1f85d0846e1f690a
Octave Lapeyronie <octave.lapeyronie@gmail.com>
2017-07-27 15:57:00 +0200

    Second commit with authentication logic up and running

--- app.js
+++ app.js
@@ -1,3 +1,4 @@
+// MIDDLEWARE
 const express = require('express');
 const path = require('path');
 const favicon = require('serve-favicon');
@@ -6,13 +7,19 @@ const cookieParser = require('cookie-parser');
 const bodyParser = require('body-parser');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
+const passport = require('passport');
+const session = require('express-session');
+const MongoStore = require('connect-mongo')(session);
+const LocalStrategy = require('passport-local').Strategy;
+const User = require('./models/user');
+const bcrypt = require('bcrypt');
 
 // CONNECT TO DB
-mongoose.connect('mongodb://localhost:207017/ironfunds-development');
+mongoose.connect('mongodb://localhost/ironfunds-development');
 
 // DEFINE ROUTES
 const index = require('./routes/index');
-const users = require('./routes/users');
+const authRoutes = require('./routes/authentication.js');
 
 // DEFINE EXPRESS APP
 const app = express();
@@ -21,7 +28,7 @@ const app = express();
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 app.set('layout', 'layouts/main-layout');
-app.use('expressLayouts');
+app.use(expressLayouts);
 
 // OTHER MIDDLEWARE
 app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
@@ -32,19 +39,102 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 
+// INITIALIZE SESSION AND PASSPORT + STRATEGIES
+app.use(session({
+  secret: 'ironfundingdev',
+  resave: false,
+  saveUninitialized: true,
+  store: new MongoStore( { mongooseConnection: mongoose.connection })
+}))
+
+passport.serializeUser((user, cb) => {
+  cb(null, user.id);
+});
+
+passport.deserializeUser((id, cb) => {
+  User.findById(id, (err, user) => {
+    if (err) { return cb(err); }
+    cb(null, user);
+  });
+});
+
+// Signing Up
+passport.use('local-signup', new LocalStrategy(
+  { passReqToCallback: true },
+  (req, username, password, next) => {
+    // To avoid race conditions
+    process.nextTick(() => {
+        User.findOne({
+            'username': username
+        }, (err, user) => {
+            if (err){ return next(err); }
+
+            if (user) {
+                return next(null, false);
+            } else {
+                // Destructure the body
+                const { username, email, description, password } = req.body;
+                const hashPass = bcrypt.hashSync(password, bcrypt.genSaltSync(8), null);
+                const newUser = new User({
+                  username,
+                  email,
+                  description,
+                  password: hashPass
+                });
+
+                newUser.save((err) => {
+                    if (err){
+                      console.log('stopped here');
+                      next(err); }
+                    return next(null, newUser);
+                });
+            }
+        });
+    });
+}));
+
+
+// Logging in
+passport.use('local-login', new LocalStrategy((username, password, next) => {
+  User.findOne({ username }, (err, user) => {
+    if (err) {
+      return next(err);
+    }
+    if (!user) {
+      return next(null, false, { message: "Incorrect username" });
+    }
+    if (!bcrypt.compareSync(password, user.password)) {
+      return next(null, false, { message: "Incorrect password" });
+    }
+
+    return next(null, user);
+  });
+}));
+
+app.use(passport.initialize());
+app.use(passport.session());
+
+// Add user to locals
+app.use((req, res, next) => {
+    res.locals.user = req.user;
+    next();
+});
+
+
 // USE ROUTES
 app.use('/', index);
-app.use('/users', users);
+app.use('/', authRoutes);
+
 
 // catch 404 and forward to error handler
-app.use(function(req, res, next) {
+app.use( (req, res, next) => {
   const err = new Error('Not Found');
   err.status = 404;
   next(err);
 });
 
 // error handler
-app.use(function(err, req, res, next) {
+app.use( (err, req, res, next) => {
   // set locals, only providing error in development
   res.locals.message = err.message;
   res.locals.error = req.app.get('env') === 'development' ? err : {};
